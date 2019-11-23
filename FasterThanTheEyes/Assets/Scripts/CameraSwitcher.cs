008e9c66444c9f59c4b005ea9c4338247cab77a2
haydn <haydn2.scarlett@live.uwe.ac.uk>
2019-11-23 20:42:19 +0000

    FixBrokenProject

---
+++ FasterThanTheEyes/Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainMenuCam;
+    [SerializeField]
+    private Camera playerCam;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mainMenuCam.enabled = true;
+        playerCam.enabled = false;
+    }
+
+    public void SwitchCams(bool playerCamera)
+    {
+        if(playerCamera)
+        {
+            mainMenuCam.enabled = false;
+            playerCam.enabled = true;
+        }
+        else
+        {
+            mainMenuCam.enabled = true;
+            playerCam.enabled = false;
+        }
+    }
+}
---
+++ FasterThanTheEyes/Assets/Scripts/SceneManagerFTTE.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerFTTE : MonoBehaviour
+{
+    private bool gameOver = false;
+    [SerializeField]
+    private Canvas mainMenuCanvas;
+    [SerializeField]
+    private Canvas gameOverCanvas;
+    [SerializeField]
+    private Canvas inGameUI;
+    [SerializeField]
+    private Fade fade;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        mainMenuCanvas.enabled = true;
+        gameOverCanvas.enabled = false;
+        inGameUI.enabled = false;
+    }
+    public void StartFade()
+    {
+        mainMenuCanvas.enabled = false;
+        fade.setFade(1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+
+    
+    void Update()
+    {
+        if(fade.loadScene)
+        {
+            LoadGame();
+        }
+        if(gameOver)
+        {
+
+            gameOverCanvas.enabled = true;
+            inGameUI.enabled = false;
+        }
+
+    }
+   private void LoadGame()
+    {
+        GetComponent<CameraSwitcher>().SwitchCams(true);
+        inGameUI.enabled = true;
+        mainMenuCanvas.enabled = false;
+        fade.setFade(0);
+    }
+
+}
