3cfdf7e62bd4ab9640c0e24130bc9dc5db905d24
Kristian Bakov <kristian2.bakov@live.uwe.ac.uk>
2019-11-25 14:48:34 +0000

    Minor fixes + start enemy attack

--- FasterThanTheEyes/Assets/Scripts/EnemyMovement.cs
+++ FasterThanTheEyes/Assets/Scripts/EnemyMovement.cs
@@ -4,24 +4,95 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float radius = -10f;
+    public float radius = 10f;
+    //circle radius
+    /// Random.insideUnitSphere * 5
     public float speed = 1f;
-    public bool offsetIsCenter = true;
+    public bool CentredToObject = true;
     public Vector3 offset;
+    //exact offset values, x is negative
     public GameObject Player;
+    //object to circle around
+
+    public float AttackCooldownCounter = 5.0f;
+    private bool coolingdown = true;
+    bool playercollided = false;
+    private bool attackOver;
 
     void Start()
     {
-        if (offsetIsCenter)
+        attackOver = true;
+        if (CentredToObject)
         {
-            offset = transform.position;
-            offset.x = -transform.position.x;
+            offset = Player.transform.position;
+            offset.x = -Player.transform.position.x;
         }
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3((radius * Mathf.Cos(Time.time * speed)) + offset.x, offset.x, (radius * Mathf.Sin(Time.time * speed)) + offset.z);
+        if (CentredToObject)
+        {
+            offset = Player.transform.position;
+            offset.x = -Player.transform.position.x;
+        }
+
+        if (!coolingdown)
+        {
+            StartCoroutine(Attack());
+        }
+        else if (attackOver)
+        {
+            transform.position = new Vector3((radius * Mathf.Cos(Time.time * speed)) + offset.x,
+                                offset.y,
+                                (radius * Mathf.Sin(Time.time * speed)) + offset.z);
+
+            AttackCooldownCounter -= Time.deltaTime;
+            if (AttackCooldownCounter <= 0)
+            {
+                coolingdown = false;
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        attackOver = false;
+        if (!playercollided)
+        {
+            transform.position = Vector3.Lerp(transform.position, Player.transform.position, Time.time * 0.01f);
+        }
+
+        if (playercollided)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3((radius * Mathf.Cos(Time.time * speed)) + offset.x,
+                                offset.y,
+                                (radius * Mathf.Sin(Time.time * speed)) + offset.z), Time.time * 0.01f);
+            yield return new WaitForSeconds(1);
+            AttackCooldownCounter = 5.0f;
+            coolingdown = true;
+            attackOver = true;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            //if enemies colllide with each other, adjust their radius
+            radius = Random.Range(radius - 5, radius + 5);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            playercollided = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playercollided = false;
+        }
     }
 
 }
