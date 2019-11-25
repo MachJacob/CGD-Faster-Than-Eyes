7c500677dfdc9f130aacbf0f3d657a5cfd0e31e2
Aaron Drinkwater <Aaron3.Drinkwater@live.uwe.ac.uk>
2019-11-25 16:10:46 +0000

    Got a basic fog kill ring around the player

--- FasterThanTheEyes/Assets/Scripts/ClearFog.cs
+++ FasterThanTheEyes/Assets/Scripts/ClearFog.cs
@@ -6,6 +6,7 @@ public class ClearFog : MonoBehaviour
 {
     //public GameObject fog_prefab;
     //public ParticleSystem fog;
+    //ParticleSystem.Particle[] particles;
     //private float destruction;
     //private float duration = 0.0f;
 
@@ -14,7 +15,12 @@ public class ClearFog : MonoBehaviour
     //{
     //    //fog = GetComponent<ParticleSystem>();
     //    //fog_prefab.GetComponentInParent<ParticleSystem>();
-    //    fog.GetComponent<ParticleSystem>();
+    //    fog.GetParticles(particles);
+
+    //    foreach (ParticleSystem.Particle particle in particles)
+    //    {
+    //        if (particle.position.)
+    //    }
     //}
 
     //// Update is called once per frame
