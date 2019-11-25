9aa1c95319f4ac6b48134f635c9eb07493c017e7
Aaron Drinkwater <Aaron3.Drinkwater@live.uwe.ac.uk>
2019-11-25 15:10:19 +0000

    Working on fog clear ring around the player.

---
+++ FasterThanTheEyes/Assets/Scripts/ClearFog.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearFog : MonoBehaviour
+{
+    //public GameObject fog_prefab;
+    //public ParticleSystem fog;
+    //private float destruction;
+    //private float duration = 0.0f;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    //fog = GetComponent<ParticleSystem>();
+    //    //fog_prefab.GetComponentInParent<ParticleSystem>();
+    //    fog.GetComponent<ParticleSystem>();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (fog)
+    //    {
+    //        //Destroy(this.fog, duration);
+    //        Destroy(fog, duration);
+    //    }
+    //}
+}
