a66bfc2058dbd4bd21f7f5edd1a65fbd036f14c7
CWardee <chriswardee@outlook.com>
2019-11-25 13:32:51 +0000

    MoreAssets+Menu

---
+++ FasterThanTheEyes/Assets/Scripts/MenuStart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuStart : MonoBehaviour
+{
+    public Camera cam;
+    bool zoomCam = false;
+    public GameObject MenuStuff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        zoomCam = true;
+        MenuStuff.SetActive(false);
+
+        //cam.orthographicSize -= 15f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (zoomCam && cam.orthographicSize > 10)
+        {
+            cam.orthographicSize -= 20f * Time.deltaTime;
+        }
+    }
+}
---
+++ FasterThanTheEyes/Assets/Scripts/Shake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public int counter = 0;
+    private int timeShake = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter < timeShake)
+        {
+            counter++;
+            this.transform.Rotate(1 * Time.deltaTime, 0, 1 * Time.deltaTime);
+        }
+
+        else if (counter < timeShake * 2)
+        {
+            counter++;
+            this.transform.Rotate(-1 * Time.deltaTime, 0, -1 * Time.deltaTime);
+        }
+
+        else
+        {
+            counter = 0;
+        }
+
+    }
+}
