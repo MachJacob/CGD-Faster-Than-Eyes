bd1ceef9a4d0debb75c723a6075ea32f768bb086
haydn <haydn2.scarlett@live.uwe.ac.uk>
2019-11-27 09:12:15 +0000

    fix

---
+++ FasterThanTheEyes/Assets/Scripts/CasterProjectile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasterProjectile : MonoBehaviour
+{
+    public float moveSpeed = 0.01f;
+    private Transform Player;
+    public GameObject target;
+    private bool ready = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = this.transform.parent.gameObject.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ready)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * moveSpeed);
+            transform.rotation = Random.rotation;
+        }
+        else if (ready)
+        {
+
+            transform.position = Vector3.Lerp(transform.position, Player.position, Time.deltaTime * 5);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //reduce Player HP
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Target")
+        {
+            Debug.Log("Launching");
+            ready = true;
+        }
+    }
+}
